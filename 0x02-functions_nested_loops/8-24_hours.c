daf0aea4702d7636fbc4fc963fea050d903b7517
njoguhmureithi <77837735+njoguhmureithi@users.noreply.github.com>
2021-06-22 12:50:52 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,12 +15,12 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-																				_putchar((i / 10) + '0');
-																				_putchar((i % 10) + '0');
-																				_putchar(':');
-																				_putchar((j / 10) + '0');
-																				_putchar((j % 10) + '0');
-																				_putchar('\n');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 																																					j++;																																	}
 		i++;
 	}
