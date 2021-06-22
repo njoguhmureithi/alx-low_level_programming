7c5356b152b37f7c4d88f75857f802a99a997569
njoguhmureithi <77837735+njoguhmureithi@users.noreply.github.com>
2021-06-22 12:44:02 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,10 +16,10 @@ int print_sign(int n)
 	} else if (n == 0)
 												{
 		_putchar(48);
-                   																	return (0);
-																		} else if (n < 0)
-																		{
-																			_putchar('-');						
-																		}
-	  																		return (-1);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');						
+	}
+		return (-1);
 }
