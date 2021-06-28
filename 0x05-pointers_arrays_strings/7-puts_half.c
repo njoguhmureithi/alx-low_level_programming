bee260b531a319334acbc1f62908bb9a62d944e7
njoguhmureithi <77837735+njoguhmureithi@users.noreply.github.com>
2021-06-28 14:18:52 +0300

    print half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,13 +19,15 @@ void puts_half(char *str)
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
-		{																	_putchar(str[i]);
+		{
+			_putchar(str[i]);
 		}
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			_putchar(str[n + 1])														}
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
