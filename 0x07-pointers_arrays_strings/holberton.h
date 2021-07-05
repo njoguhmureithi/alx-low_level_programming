17cc15f000e67156990628dfe1346c6e3fade59a
njoguhmureithi <77837735+njoguhmureithi@users.noreply.github.com>
2021-07-05 19:39:22 +0300

    header file

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,6 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
-
+// prototype files
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
